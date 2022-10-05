94e79618784764d685c796fb1e15189078925501
Abidemi Ope <opeabidemi@gmail.com>
2022-10-05 15:41:12 +0100

    new lesson

--- src/com/abidemiope/lesson3/Main.java
+++ src/com/abidemiope/lesson3/Main.java
@@ -2,66 +2,64 @@ package com.abidemiope.lesson3;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class Main extends Frame {
 
     public Main() {
-//        JTextArea textArea = new JTextArea("Write a comment");
-//
-//        textArea.setBounds(10, 50, 200, 200);
-//
-//        JLabel label = new JLabel("0");
-//        label.setBounds(10, 10, 200, 50);
-//
-//        textArea.addKeyListener(new KeyListener() {
-//            @Override
-//            public void keyTyped(KeyEvent e) {
-//
-//            }
-//
-//            @Override
-//            public void keyPressed(KeyEvent e) {
-//
-//            }
-//
-//            @Override
-//            public void keyReleased(KeyEvent e) {
-//                label.setText(String.valueOf(textArea.getText().length()));
-//            }
-//        });
-//
-//        add(label);
-//        add(textArea);
-//
-//        JCheckBox checkBox = new JCheckBox("Red", true);
-//        checkBox.setBounds(10, 300, 100, 50);
-//        add(checkBox);
-//
-//        checkBox.addActionListener(e -> System.out.println(checkBox.isSelected()));
+        JTextArea textArea = new JTextArea("Write a comment");
 
-//        JRadioButton r1=new JRadioButton("A) Male");
-//        JRadioButton r2=new JRadioButton("B) Female");
-//        r1.setBounds(75,50,100,30);
-//        r2.setBounds(75,100,100,30);
-//
-//        ButtonGroup gender = new ButtonGroup();
-//        gender.add(r1);
-//        gender.add(r2);
-//
-//        add(r1);
-//        add(r2);
+        textArea.setBounds(10, 50, 200, 200);
 
-//        String[] names = {"James", "Peter", "David", "John"};
+        JLabel label = new JLabel("0");
+        label.setBounds(10, 10, 200, 50);
 
-//        JComboBox select = new JComboBox(names);
-//
-//        select.setBounds(50, 50, 200, 50);
-//
-//        add(select);
+        textArea.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                label.setText(String.valueOf(textArea.getText().length()));
+            }
+        });
+
+        add(label);
+        add(textArea);
+
+        JCheckBox checkBox = new JCheckBox("Red", true);
+        checkBox.setBounds(10, 300, 100, 50);
+        add(checkBox);
+
+        checkBox.addActionListener(e -> System.out.println(checkBox.isSelected()));
+
+        JRadioButton r1=new JRadioButton("A) Male");
+        JRadioButton r2=new JRadioButton("B) Female");
+        r1.setBounds(75,50,100,30);
+        r2.setBounds(75,100,100,30);
+
+        ButtonGroup gender = new ButtonGroup();
+        gender.add(r1);
+        gender.add(r2);
+
+        add(r1);
+        add(r2);
+
+        String[] names = {"James", "Peter", "David", "John"};
+
+        JComboBox select = new JComboBox(names);
+
+        select.setBounds(50, 50, 200, 50);
+
+        add(select);
 
         String[][] students = {
                 {"1", "Onome", "Java"},
---
+++ src/com/abidemiope/lesson4/Main.java
@@ -0,0 +1,81 @@
+package com.abidemiope.lesson4;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main extends Frame {
+
+    public String[][] students = {
+            {"1", "Onome", "Java"},
+            {"2", "Ebuka", "C#"},
+            {"3", "Eniola", "React" },
+            {"4", "Wisdom", "Node"},
+            {"5", "Vera", "R"},
+            {"6", "Ifeoma", "C"},
+            {"7", "Tochukwu", "SQL"}
+    };
+
+    public String[] headings = {"ID", "NAME", "COURSE"};
+    public int selectedColumn = -1;
+    public int temp = -1;
+    public JTable table;
+    public  ListSelectionModel listSelectionModel;
+
+    public Main() {
+
+        JLabel label = new JLabel("List of Students");
+        label.setBounds(10, 20, 300, 50);
+
+        add(label);
+
+        table = new JTable(students, headings);
+        table.setBounds(10,70, 400, 50 * students.length );
+        table.setRowHeight(50);
+
+        listSelectionModel = table.getSelectionModel();
+
+        listSelectionModel.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        listSelectionModel.addListSelectionListener((e) -> {
+            if(selectedColumn == table.getSelectedRow() || table.getSelectedRow() == temp){
+
+            }else if(selectedColumn != -1) {
+                // runs when a row has been prev' selected
+                switchRows(selectedColumn, table.getSelectedRow());
+                temp = table.getSelectedRow();
+                selectedColumn = -1;
+            }else {
+                selectedColumn = table.getSelectedRow();
+            }
+        });
+
+        add(table);
+
+        setSize(new Dimension(800, 600));
+        setLayout(null);
+        setVisible(true);
+    }
+
+    public void switchRows(int row1, int row2){
+        String[] row1Data = students[row1];
+        String[] row2Data = students[row2];
+
+        String ID = row1Data[0];
+        String NAME = row1Data[1];
+        String COURSE = row1Data[2];
+
+        // For ROW 1
+        table.setValueAt(row2Data[0], row1, 0);
+        table.setValueAt(row2Data[1], row1, 1);
+        table.setValueAt(row2Data[2], row1, 2);
+
+        // For ROW2
+        table.setValueAt(ID, row2, 0);
+        table.setValueAt(NAME, row2, 1);
+        table.setValueAt(COURSE, row2, 2);
+
+    }
+
+    public static void main(String[] args) {
+        new Main();
+    }
+}
